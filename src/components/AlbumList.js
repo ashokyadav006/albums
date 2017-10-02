861f14fd2c6e3d0df98ec314dae785225297252d
Ashok Yadav <yadaas@amazon.com>
2017-10-03 00:23:16 +0530

    Added axios

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList =  () => {
-	return (
-		<View>
-			<Text>Album List!!!!</Text>
-		</View>
-	);
-};
+
+class AlbumList extends Component {
+
+	state = { albums: [] };
+
+	componentWillMount() {
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+		.then(response => {
+			this.setState({albums: response.data});
+		})
+	}
+
+
+	render() {
+		console.log(this.state);
+		return (
+			<View>
+				<Text>Album List!!!!</Text>
+			</View>
+		);
+	}
+}
 
 export default AlbumList;
